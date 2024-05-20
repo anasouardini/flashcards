bf063d62a786697572101daf0f2738a2efe87fc5
anasouardini <ouardinianas@proton.me>
2024-05-20 15:22:37 +0100

    feat: add history and stats support

--- src/model/model.ts
+++ src/model/model.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 export type CardSide = "front" | "back";
 export interface CardFrontSide {
-    content: string | string[]
+    content: string;
 }
 export interface CardBackSide {
     content: string | string[]
@@ -16,8 +16,45 @@ export interface Level {
         back: CardBackSide
     }[]
 }
+
+export interface ActionAdd {
+    type: "add",
+    word: string,
+    date: string,
+}
+export interface ActionMove {
+    type: "move",
+    word: string,
+    fromLevel: number,
+    toLevel: number,
+    date: string,
+}
+export interface ActionTrash {
+    type: "trash",
+    word: string
+    date: string,
+}
+export interface ActionEdit {
+    type: "edit",
+    word: string
+    date: string,
+}
+
+export type ActionHistory = ActionAdd | ActionMove | ActionTrash | ActionEdit
+
+export interface LevelHistory {
+    lengths: {
+        length: number,
+        date: string
+    }[],
+}
+
 export interface Model {
     currentLevel: number,
+    history: {
+        actions: (ActionAdd | ActionMove | ActionTrash | ActionEdit)[],
+        levels: LevelHistory[]
+    },
     levels: Level[]
 }
 
@@ -27,6 +64,25 @@ const vars = {
         data: path.join(__dirname, "..", "data"),
         currentFile: ""
     }
+}
+
+function addNewProperties(obj: Model) {
+    if (!obj.history) {
+        obj.history = {
+            actions: [],
+            levels: []
+        };
+    }
+}
+
+export function recordHistory(action: ActionAdd | ActionMove | ActionTrash | ActionEdit) {
+    // vars.model.history.actions.push(action);
+    // vars.model.history.levels.forEach((level, index) => {
+    //     level.lengths.push({
+    //         length: vars.model.levels[index].cards.length,
+    //         date: new Date().toISOString()
+    //     });
+    // })
 }
 
 export function loadList({ item }: { item: string }) {
@@ -42,6 +98,7 @@ export function loadList({ item }: { item: string }) {
 
     try {
         vars.model = JSON.parse(content);
+        addNewProperties(vars.model);
     } catch (E) {
         console.log(`data/${item} is not valid`);
     }
