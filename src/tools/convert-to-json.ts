0200d6d11d1766735280489b4abdcecb9a6366cc
anasouardini <ouardinianas@proton.me>
2024-05-20 15:22:51 +0100

    fix: trim word before saving

--- src/tools/convert-to-json.ts
+++ src/tools/convert-to-json.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { type Model, type Level, CardFrontSide, CardBackSide } from '../model/model';
+import { type Model, type LevelHistory, CardFrontSide, CardBackSide } from '../model/model';
 
 const inputFilePath = path.join(__dirname, '..', 'data', 'french-voc.md');
 const outputFilePath = path.join(__dirname, '..', 'data', 'french-voc.json');
@@ -25,7 +25,7 @@ wordBlobs.forEach(wordBlob => {
     const word = lines[0];
     const examples = lines.slice(1).filter((line) => { return line != "" })
 
-    const cardFront: CardFrontSide = { content: word.includes("#") ? word.slice(1) : word };
+    const cardFront: CardFrontSide = { content: word.includes("#") ? word.slice(1).trim() : word.trim() };
     const cardBack: CardBackSide = { content: examples.map((line)=>{return line.trim()}) };
 
     model.levels[word.includes("#") ? 1 : 0].cards.push({ front: cardFront, back: cardBack });
