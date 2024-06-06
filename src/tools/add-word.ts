c066a0456b1f74d5ffb9cc537c2a621d5d9f7262
anasouardini <ouardinianas@proton.me>
2024-06-06 21:14:02 +0100

    feat: blessed is so buggy, adding a tool for inserting new words

---
+++ src/tools/add-word.ts
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+import { type Model } from '../model/model';
+
+const args = {
+  wordFR: process.argv[2],
+  wordEN: process.argv[3],
+  definitionEN: process.argv[4],
+};
+
+const wordsList = path.join(__dirname, '..', 'data', 'french-voc.json');
+const content = fs.readFileSync(wordsList, 'utf-8');
+
+let model: Model = JSON.parse(content);
+model.levels[0].cards.push({
+  front: { content: args.wordFR },
+  back: { content: `- [${args.wordEN}]: ${args.definitionEN ?? ''}` },
+});
+
+fs.writeFileSync(wordsList, JSON.stringify(model, null, 2));
