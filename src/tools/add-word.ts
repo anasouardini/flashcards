b8ce6f3d6efecb4520fe6d510c035011278717d3
anasouardini <ouardinianas@proton.me>
2024-09-29 06:52:22 +0100

    chore: better tooling

--- src/tools/add-word.ts
+++ src/tools/add-word.ts
@@ -1,20 +1,53 @@
-import fs from 'fs';
-import path from 'path';
-import { type Model } from '../model/model';
+import { type Model, loadList, saveList, getModel, recordHistory } from '../model/model';
+import tools from './tools';
 
 const args = {
-  wordFR: process.argv[2],
-  wordEN: process.argv[3],
-  definitionEN: process.argv[4],
+  dictionaryName: process.argv[2],
+  foreignWord: process.argv[3],
+  equivalentWord: process.argv[4],
+  definitionEN: process.argv[5],
+  forms: process.argv[6],
 };
 
-const wordsList = path.join(__dirname, '..', 'data', 'french-voc.json');
-const content = fs.readFileSync(wordsList, 'utf-8');
+loadList({ item: `${args.dictionaryName}.json` });
+let model = getModel();
+if (!model) {
+  process.exit(2);;
+}
 
-let model: Model = JSON.parse(content);
-model.levels[0].cards.push({
-  front: { content: args.wordFR },
-  back: { content: [`- [${args.wordEN}]: ${args.definitionEN ?? ''}`] },
+const alreadyExists = model.model.levels.some((level) => {
+  return level.cards.some((card) => {
+    const existingWord = card.front.content.split(' ')[0].trim().toLowerCase();
+    const newWord = args.foreignWord.split(' ')[0].trim().toLowerCase();
+    return existingWord == newWord;
+  })
+})
+if (alreadyExists) {
+  console.log('Word already exists');
+  process.exit(1);
+}
+
+model.model.levels[0].cards.push({
+  front: { content: tools.capitalize(args.foreignWord) },
+  back: {
+    content: [
+      `- [${tools.capitalize(args.equivalentWord)}]: ${args.definitionEN ?? ''}`,
+      `- Forms: ${args.forms ?? ''}`,
+    ]
+  },
 });
 
-fs.writeFileSync(wordsList, JSON.stringify(model, null, 2));
+recordHistory({
+  type: 'add',
+  word: tools.capitalize(args.foreignWord.split(' ')[0].trim()),
+  date: new Date().toISOString(),
+});
+
+saveList();
+
+const total = model.model.levels.reduce((total, level) => {
+    return level.cards.length + total;
+  }, 0)
+console.log({
+  total,
+})
